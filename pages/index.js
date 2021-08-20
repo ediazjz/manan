abc9acf504126b1fc2c079656d9af2e7de693865
ediazjz <ediazjz@outlook.com>
2021-08-20 18:36:18 -0500

    Add Container Padding and Auto Center

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
 
-      <main>
+      <main className="container">
         <SocialLogIn network="google" />
         <SocialLogIn network="facebook" />
         <SocialLogIn network="twitter" />
