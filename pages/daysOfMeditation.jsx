f8d6b1f49270f8fc6d839aabfb2c2bf6d21e7918
ediazjz <ediazjz@outlook.com>
2021-09-11 22:16:40 -0500

    Add Remaining Page Routes

---
+++ pages/daysOfMeditation.jsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import { Layout } from '../components'
+
+const DaysOfMeditation = () => {
+  return (
+    <Layout>
+      <Head>
+        <title>Manan | #100DaysOfMeditation</title>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+
+      <h1>This will be the #100Days Page</h1>
+    </Layout>
+  )
+}
+
+export default DaysOfMeditation
