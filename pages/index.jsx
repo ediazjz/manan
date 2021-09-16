bcebba44658cebe25e3345a1a208e48ba5571628
ediazjz <ediazjz@outlook.com>
2021-09-16 01:00:34 -0500

    Add Layout and Styles

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,10 @@
-import Script from 'next/script'
-import Head from 'next/head'
 import { useEffect } from 'react'
-import { Illustration } from '../components'
+import Head from 'next/head'
+import Script from 'next/script'
+
+import { DownloadIcon, LoginIcon } from '@heroicons/react/outline'
+
+import { Button, Illustration, Logo } from '../components'
 
 export default function Home() {
   useEffect(() => {
@@ -13,16 +16,55 @@ export default function Home() {
     ).start();
   }, [])
 
+  const downloadApp = () => {
+    console.log("hey")
+  }
+
   return (
     <>
       <Head>
-        <title>manan</title>
+        <title>manan:</title>
 
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
 
       <Script src="https://cdn.jsdelivr.net/npm/kute.js@2.1.2/dist/kute.min.js" strategy="beforeInteractive" />
-      <Illustration width="600" />
+
+      <main className="container flex flex-col lg:flex-row justify-center items-center h-screen">
+        <div className="flex flex-col items-center lg:order-2 lg:w-1/2 mb-12 lg:mb-0">
+          <Illustration className="w-3/4 md:w-1/2 lg:w-3/4" />
+
+          <Logo type="logotype" className="w-3/4 md:w-1/2 mt-8 text-primary" />
+        </div>
+
+        <div className="flex flex-col lg:order-1 lg:w-1/2">
+          <div className="mb-12 text-center">
+            <h1 className="h3 mb-2 text-primary">
+              Meditate, share it and get motivated
+            </h1>
+
+            <p>
+              The perfect place to keep track of your meditative practice.
+            </p>
+          </div>
+
+          <div className="flex flex-col items-center md:w-max md:mx-auto">
+            <Button
+              className="btn-primary w-full mb-4 shadow-lg"
+              href="/login"
+              text="Start now"
+              icon={<LoginIcon className="btn-icon" />}
+            />
+
+            <Button
+              className="btn-secondary w-full"
+              onClick={downloadApp}
+              text="Download App"
+              icon={<DownloadIcon className="btn-icon" />}
+            />
+          </div>
+        </div>
+      </main>
     </>
   )
 }
